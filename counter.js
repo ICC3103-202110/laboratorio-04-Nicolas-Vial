8228a0625735b2e4ab80096a1dbf6d7aaa49e7d5
Nicolas-Vial <navial@miuandes.cl>
2021-05-04 16:10:23 -0400

    final

--- counter.js
+++ counter.js
@@ -2,21 +2,31 @@
 var prompt = require('prompt-sync')();
 
 function view(counter){
-  return '\nCount: '+counter.toString()+'\n\n (+) (-)\n\n\n(q) for quit\n\nWhat would you do? ';
+  return '\nCount: '+counter.toString()+'\n\n (+) (-)\n\n\n(q) for quit\n\n';
 }
 
-/*function update(msg, counter){
+function update(msg, counter){
+  if(msg==='+'){
+    counter+=1;
+    return counter;
+  }else if(msg==='-'){
+    counter-=1;
+    return counter;
+  }else{
+    return counter;
+  }
 
-}*/
+}
 
 function  app(count){
   let counter=count;
-  let input;
-  while(input!='q'){
+  let msg;
+  while(msg!='q'){
     const currentView=view(counter);
-    console.log(currentView)
-    input=prompt('');
-    
+    console.clear()
+    console.log(currentView);
+    msg=prompt('What would you do? ');
+    counter=update(msg, counter);
   }
 }
 
